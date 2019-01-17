ee019b5bc6642e5568259c0ad091e980bde559e0
skanny999 <skanny999@me.com>
2019-01-17 21:56:50 +0000

    Initial Commit

---
+++ PoliceTests/PoliceTests.swift
@@ -0,0 +1,34 @@
+//
+//  PoliceTests.swift
+//  PoliceTests
+//
+//  Created by Riccardo on 17/01/2019.
+//  Copyright © 2019 RiccardoScanavacca. All rights reserved.
+//
+
+import XCTest
+@testable import Police
+
+class PoliceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
