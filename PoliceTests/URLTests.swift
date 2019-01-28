7ecf67050828e0154acd73721e727f0fb86e8aa9
skanny999 <skanny999@me.com>
2019-01-28 19:53:55 +0000

    Minor refactor

--- PoliceTests/URLTests.swift
+++ PoliceTests/URLTests.swift
@@ -19,7 +19,6 @@ class URLTests: XCTestCase {
     override func setUp() {
         
         createLocation()
-
     }
     
     func testURLsWithStandardLocation() {
@@ -52,17 +51,6 @@ class URLTests: XCTestCase {
         location = nil
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
     
     func allURLs() -> [URL] {
         
