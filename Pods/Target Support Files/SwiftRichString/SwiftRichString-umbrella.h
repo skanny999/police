181400a5b72e700ae7b61b3e5bd88e330780e053
skanny999 <skanny999@me.com>
2019-04-22 18:28:55 +0100

    Refactor

--- Pods/Target Support Files/SwiftRichString/SwiftRichString-umbrella.h
+++ Pods/Target Support Files/SwiftRichString/SwiftRichString-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_PoliceUITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_PoliceUITestsVersionString[];
+FOUNDATION_EXPORT double SwiftRichStringVersionNumber;
+FOUNDATION_EXPORT const unsigned char SwiftRichStringVersionString[];
 
